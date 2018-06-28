29d8955c04fe7f3d097e3d8afd5f7b3edbb65ae0
Alexandre Siborro <alexandre.siborro@zalando.pt>
2018-06-28 22:52:09 +0100

    Update mock server data

--- src/reducers/article.js
+++ src/reducers/article.js
@@ -1,38 +1,10 @@
-import { combineReducers } from 'redux';
-import { createSelector } from 'reselect';
-import { makeHashReducer, makeArrayReducer } from './helper';
 import * as articleActions from '../actions/article';
 
-const initialState = {
-  byId: {},
-  allIds: []
-};
-
-export default combineReducers({
-  byId: makeHashReducer(
-    initialState.byId,
-    'id',
-    'article',
-    [articleActions.ADD_ARTICLE],
-    [articleActions.REMOVE_ARTICLE],
-    'data.entities.article',
-    articleActions.LOAD_ARTICLES
-  ),
-  allIds: makeArrayReducer(
-    initialState.allIds,
-    'article.id',
-    articleActions.ADD_ARTICLE,
-    articleActions.REMOVE_ARTICLE,
-    'data.result.items',
-    articleActions.LOAD_ARTICLES
-  )
-});
-
+export default function (state = [], action) {
+  if (action.type === articleActions.LOAD_ARTICLES) {
+    return action.articles;
+  }
+  return state;
+}
 
 export const articlesSelector = state => state.article;
-
-export const allArticlesSelector = createSelector(
-  articlesSelector,
-  articles =>
-    articles.allIds.map(articleId => articles.byId[articleId])
-);
