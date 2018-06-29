e603e5847b88f6bd286ccaab067564047fc996ba
Alexandre Siborro <alexandre.siborro@zalando.pt>
2018-06-29 09:29:03 +0100

    Cleanup

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,24 @@
-import { call } from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
+import fetchArticles from '../api/article';
+import * as articleActions from '../actions/article';
 
-import articleSaga from './article';
+/* Article Entity
+ *
+ * const ARTICLE = {
+ *   id: 3,
+ *   name: 'Article 3',
+ *   description: 'Description 3',
+ *   price: 19.99,
+ *   stock: 10
+ * };
+ */
 
+// TODO: fetch all articles and publish them to the store
+// eslint-disable-next-line no-empty-function
 export default function* () {
-  yield call(articleSaga);
+  const result = yield call(fetchArticles);
+
+  if (result.items) {
+    yield put(articleActions.loadArticles(result.items));
+  }
 }
