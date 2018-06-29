b0458a3d994a0fbd3dbf584ffb40324cd1846768
Alexandre Siborro <alexandre.siborro@zalando.pt>
2018-06-29 08:52:07 +0100

    Remove unnused files

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,39 @@
-import { call } from 'redux-saga/effects';
+import { all, call, put, takeEvery } from 'redux-saga/effects';
+import fetchArticles from '../api/article';
+import * as articleActions from '../actions/article';
+import * as cartActions from '../actions/cart';
 
-import articleSaga from './article';
+/* Article Entity
+ *
+ * const ARTICLE = {
+ *   id: 3,
+ *   name: 'Article 3',
+ *   description: 'Description 3',
+ *   price: 19.99,
+ *   stock: 10
+ * };
+ */
+
+function* fetchArticlesSaga() {
+  const result = yield call(fetchArticles);
+
+  if (result.items) {
+    yield put(articleActions.loadArticles(result.items));
+  }
+}
+
+function* updateArticleStockSaga(inc, action) {
+  if (inc) {
+    yield put(articleActions.incStock(action.id));
+  } else {
+    yield put(articleActions.decStock(action.id));
+  }
+}
 
 export default function* () {
-  yield call(articleSaga);
+  yield all([
+    call(fetchArticlesSaga),
+    takeEvery(cartActions.REMOVE_ITEM, updateArticleStockSaga, true),
+    takeEvery(cartActions.ADD_ITEM, updateArticleStockSaga, false)
+  ]);
 }
